16d0d4cb80c58721687ebeeb03c39bcb135580d3
Benyapha <s6502041620069@email.kmutnb.ac.th>
2025-03-03 12:44:43 +0700

    update

--- server.js
+++ server.js
@@ -38,6 +38,52 @@ app.post('/user', async (req, res) => {
     }
 });
 
+// Update user
+app.put('/user/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { username, password } = req.body;
+
+        let updateData = {};
+        if (username) updateData.username = username;
+        if (password) updateData.password = password;
+        const response = await prisma.user.update({
+            where: { id: Number(id) },
+            data: updateData
+        });
+
+        res.json({
+            message: "User updated successfully",
+            user: response
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating user",
+            error: error.message
+        });
+    }
+});
+
+// DEl ID
+app.delete('/user/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        await prisma.user.delete({
+            where: { id: Number(id) }
+        });
+
+        res.json({
+            message: "User deleted successfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error deleting user",
+            error: error.message
+        });
+    }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
